061ca12f320c049cf2b3eaec6e548186314c3ce1
Alice Xu <61668791+alice-xuu@users.noreply.github.com>
2022-04-02 19:29:46 +1100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from modules import *
+from routes import *
+
+# Starting the python applicaiton
+if __name__ == '__main__':
+    PORT_NUMBER = 5000
+
+    print("-"*70)
+    print("""Welcome to Media Server Backend.\n
+             Please open your browser to:
+             http://127.0.0.1:{}""".format(PORT_NUMBER))
+    print("-"*70)
+    app.run(debug=True, host='0.0.0.0', port=PORT_NUMBER)
---
+++ run.command
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Change to current directory and run
+#cd "assignment_webapp "$0"`"
+python3 main.py
